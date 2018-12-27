8819373d5253133cd51e199538b46e8a680d9d36
anmolrajaroraa <anmolarora1711@gmail.com>
2018-12-27 09:06:36 +0530

    Create main.js

---
+++ part1/scripts/main.js
@@ -0,0 +1,4 @@
+window.addEventListener("load",()=>{
+    var myHeading = document.querySelector("h1"); //part of Document Object Model API (DOM)
+    myHeading.textContent = "Hello, world!";        //DOM allows you to manipulate docuements
+})
