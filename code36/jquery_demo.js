0995e5c36867f983b8b9f2e99edb889a381b292b
Anmol Arora <anmolarora1711@gmail.com>
2019-02-03 11:08:34 +0530

    Add files via upload

---
+++ code36/jquery_demo.js
@@ -0,0 +1,16 @@
+//$(document).ready(function(){
+    $(function(){
+        $(":button").click(function(){
+            //$(this).hide();
+            //$("p").hide();
+            //$("h1.first").hide();
+            //$("#first").hide();
+            //$("*").hide();
+            //$("p:first").hide();
+            //$("ul li:first").hide();
+            //$("ul li:first-child").hide();
+            //$("[href]").hide();
+            //$("a[target!='_blank']").hide();
+            $("tr:even").hide();
+        })
+    })
