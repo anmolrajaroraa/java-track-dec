3129e9bcc080ab827329911dd3c81d03ad0391db
Anmol Arora <anmolarora1711@gmail.com>
2019-02-09 17:12:13 +0530

    Update jquery_demo.js

--- part36/jquery_demo.js
+++ part36/jquery_demo.js
@@ -1,16 +1,50 @@
 //$(document).ready(function(){
     $(function(){
-        $(":button").click(function(){
+        $(":button").dblclick(function(){
             //$(this).hide();
             //$("p").hide();
             //$("h1.first").hide();
             //$("#first").hide();
-            //$("*").hide();
+            $("*").hide();
             //$("p:first").hide();
             //$("ul li:first").hide();
             //$("ul li:first-child").hide();
             //$("[href]").hide();
             //$("a[target!='_blank']").hide();
-            $("tr:even").hide();
+            //$("tr:even").hide();
+        });
+
+        $("p").mouseup(function(){
+            alert("You exited from some paragraph");
+        });
+        
+        $("p").hover(function(){
+            $(this).css("background-color","yellow");
+        },function(){
+            $(this).css("background-color","white");
+        });
+        
+        /*$("input").focus(function(){
+            $(this).css("background-color","#ddd");
+        });
+        $("input").blur(function(){
+            $(this).css("background-color","white");
+        });*/
+        
+        $("p").on("click",function(){
+            $(this).hide();
+        });
+
+        $("input").on({
+            mouseenter : function(){
+                $(this).css("background-color","#ddd")
+            },
+            mouseleave : function(){
+                $(this).css("background-color","#1bde32");
+            },
+            focus : function(){
+                $(this).css("border","5px yellow solid");
+            }
         })
+
     })
