920f3db708d312ef3360fae3cda0f1c10eeafa13
Anmol Arora <anmolarora1711@gmail.com>
2019-02-09 17:22:10 +0530

    Add files via upload

---
+++ part38/directive.js
@@ -0,0 +1,10 @@
+app.directive("myDirective",function(){
+    return{
+        template: "I am a custom directive"
+    }
+});
+app.directive("secondDirective",function(){
+    return{
+        template: "Second directive"
+    }
+})
