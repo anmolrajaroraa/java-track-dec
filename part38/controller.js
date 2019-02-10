454099b3f87111776de7a1e0f4aa66d7f8dbbafe
Anmol Arora <anmolarora1711@gmail.com>
2019-02-10 11:28:31 +0530

    Update controller.js

--- part38/controller.js
+++ part38/controller.js
@@ -1,4 +1,13 @@
 app.controller('myCtrl',function($scope){
     $scope.firstname = "John";
     $scope.lastname = "Smith";
+    $scope.changeName = function(){
+        $scope.firstname = "Emily";
+    }
+    $scope.arr = ["Mike", "John" ,"Emily"]
+    /*$scope.persons = [
+        {name:"John", country:"Norway"},
+        {name:"Emily", country:"Sweden"},
+        {name:"Mike", country:"Finland"}
+    ]*/
 })
