6a5043ec69d5abb95f51affa79e50ebb82ff4dd4
Anmol Arora <anmolarora1711@gmail.com>
2019-03-10 16:18:58 +0530

    Add files via upload

--- hibernate/src/org/mycompany/dto/UserDetails.java
+++ hibernate/src/org/mycompany/dto/UserDetails.java
@@ -1,10 +1,16 @@
 package org.mycompany.dto;
 
+import java.util.Date;
+
+import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity (name="USER_DETAILS")
+//@Entity (name="USER_DETAILS")
+@Entity
+@Table (name="USER_DETAILS")
 public class UserDetails {
 	
 	@Id
@@ -12,7 +18,22 @@ public class UserDetails {
 	private int userId;
 	
 	@Column (name="USER_NAME")
+	@Basic
 	private String userName;
+	
+	@Column (name="USER_ADDRESS")
+	private String address;
+	
+	@Column (name="USER_DESCRIPTION")
+	private String description;
+	
+	@Column (name="USER_JOINING_DATE")
+	private Date joiningDate;
+	
+	private static String companyName = "mycompany";
+	
+	@Column (name="NOT_TO_BE_PERSISTED")
+	private String doNotSaveMe;
 	
 	public int getUserId() {
 		return userId;
@@ -26,4 +47,40 @@ public class UserDetails {
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Date getJoiningDate() {
+		return joiningDate;
+	}
+	public void setJoiningDate(Date joiningDate) {
+		this.joiningDate = joiningDate;
+	}
+	public static String getCompanyName() {
+		return companyName;
+	}
+	public static void setCompanyName(String companyName) {
+		UserDetails.companyName = companyName;
+	}
+	public String getDoNotSaveMe() {
+		return doNotSaveMe;
+	}
+	public void setDoNotSaveMe(String doNotSaveMe) {
+		this.doNotSaveMe = doNotSaveMe;
+	}
+	@Override
+	public String toString() {
+		return "UserDetails [userId=" + userId + ", userName=" + userName + ", address=" + address + ", description="
+				+ description + ", joiningDate=" + joiningDate + ", doNotSaveMe=" + doNotSaveMe + "]";
+	}
+	
 }
--- hibernate/src/org/mycompany/hibernate/HibernateTest.java
+++ hibernate/src/org/mycompany/hibernate/HibernateTest.java
@@ -1,5 +1,7 @@
 package org.mycompany.hibernate;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -10,13 +12,35 @@ public class HibernateTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		UserDetails user = new UserDetails();
-		user.setUserId(3);
-		user.setUserName("Third user");
+		user.setUserId(1);
+		user.setUserName("First user");
+		user.setAddress("First user's address");
+		user.setDescription("Description goes here");
+		user.setJoiningDate(new Date());
+		user.setDoNotSaveMe("Nothing");
 		
+		System.out.println(user.toString());
+		System.out.println(UserDetails.getCompanyName());
+		
+		//Object obj = new Object();
+		//obj.toString();
+		//Object obj = user;
+		
+		UserDetails user2 = new UserDetails();
+		user2.setUserId(2);
+		user2.setUserName("Second user");
+		user2.setAddress("Second user's address");
+		user2.setDescription("Description again goes here");
+		user2.setJoiningDate(new Date());
+		user2.setDoNotSaveMe("Nothing again");
+		
+		System.out.println(user2.toString());
+		System.out.println(UserDetails.getCompanyName());
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 		session.save(user);
+		session.save(user2);
 		session.getTransaction().commit();
 	}
 
